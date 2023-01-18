7f6d9003dfb6c649af2462e7cdfe062f2bb2c55b
jaguar48 <okonkwostanley67@yahoo.com>
2023-01-18 22:26:09 +0100

    Add project files.

---
+++ Elastic collection/Program.cs
@@ -0,0 +1,27 @@
+﻿using Elastic_collection;
+using System;
+using System.Collections.Generic;
+using System.Dynamic;
+
+class Program
+{
+    static void Main()
+    {
+        MyClass myc = new MyClass();
+        var products = new List<dynamic>
+        {
+           
+            MyClass.CreateProduct(1, "Dell Xps", 30, 1500, "PCs", 1000),
+            MyClass.CreateProduct(2, "Ergonomic Chair", 400, 200, "Chairs", 4000),
+            MyClass.CreateProduct(3, "Table", 500, 250, "Tables", 3000),
+        };
+        string[] properties = MyClass.GetProperties();
+        if (properties.Length == 0)
+        {
+            properties = new string[] { "Id", "Name", "Quantity", "Price", "Category", "OrderCount" };
+        }
+        MyClass.ListProducts(products, properties);
+    }
+
+  
+}
---
+++ Elastic collection/product.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+
+
+namespace Elastic_collection
+{
+    public class MyClass
+
+    {
+        public static dynamic CreateProduct(int id, string name, int quantity, decimal price, string category, int orderCount)
+        {
+            dynamic product = new ExpandoObject();
+            product.Id = id;
+            product.Name = name;
+            product.Quantity = quantity;
+            product.Price = price;
+            product.Category = category;
+            product.OrderCount = orderCount;
+            return product;
+        }
+
+        public static void ListProducts(List<dynamic> products, string[] properties)
+        {
+            foreach (dynamic product in products)
+            {
+                foreach (var property in product)
+                {
+                    if (Array.IndexOf(properties, property.Key) == -1) continue;
+                    Console.Write(property.Key + ": " + property.Value + ", ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public static string[] GetProperties()
+        {
+            Console.WriteLine("Enter the properties you want to display, separated by commas:");
+            var input = Console.ReadLine();
+            string[] properties = input.Split(',');
+            return properties;
+        }
+    }
+}
